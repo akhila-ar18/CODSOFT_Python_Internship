305571aee5380f7c0461fd5526e6bdbb21c6ad21
AKHILA A R <107562708+akhila-ar18@users.noreply.github.com>
2024-03-02 18:53:12 +0530

    passwordGenerator.py

---
+++ Password_Generator/passwordGenerator.py
@@ -0,0 +1,31 @@
+import random
+import string
+
+def generate_password(length):
+    # This line is used to define the characters to be used in the password
+    characters = string.ascii_letters + string.digits + string.punctuation
+
+    # This line generates the password using random.choice to select characters randomly
+    password = ''.join(random.choice(characters) for i in range(length))
+    return password
+
+def main():
+    while True:
+        # This line Prompts the user to specify the desired length of the password
+        length = int(input("Enter the desired length of the password: "))
+
+        # This line generates the password
+        password = generate_password(length)
+
+        # This line displays the generated password
+        print("Generated Password:", password)
+
+        choice = input("Do you still wanna continue(1/0)? ")
+        if(choice != "1"):
+            print("Exiting...")
+            break
+        else:
+            continue
+
+if __name__ == "__main__":
+    main()
